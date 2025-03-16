0dce2d1b39879b8b39010f54602d3c2f736016ea
Андрей <133362636+AndrewOsipov1@users.noreply.github.com>
2025-03-16 17:24:19 +0300

    Add files via upload

--- fueltruck.py
+++ fueltruck.py
@@ -1,23 +1,22 @@
 import threading
 import json
 import time
-from flask import Flask
+from flask import Flask, render_template, request
 import requests
 from collections import defaultdict
 
-ip_plane = '1.1.1.1'    # IP адрес сервера самолета (ОБЯЗАТЕЛЬНО ЗАМЕНИТЬ НА РЕАЛЬНЫЙ)
-ip_uno = '1.1.1.1'      # IP адрес сервера УНО  (ОБЯЗАТЕЛЬНО ЗАМЕНИТЬ НА РЕАЛЬНЫЙ)
-ip_dr = '0.0.0.0'       # IP адрес сервера диспетчера руления (ОБЯЗАТЕЛЬНО ЗАМЕНИТЬ НА РЕАЛЬНЫЙ)
-is_testmode = True     # Флаг тестового режима
+ip_plane = '192.168.35.22'    # IP адрес сервера самолета (ОБЯЗАТЕЛЬНО ЗАМЕНИТЬ НА РЕАЛЬНЫЙ)
+ip_uno = '192.168.35.125'      # IP адрес сервера УНО  (ОБЯЗАТЕЛЬНО ЗАМЕНИТЬ НА РЕАЛЬНЫЙ)
+ip_dr = '192.168.35.219'       # IP адрес сервера диспетчера руления (ОБЯЗАТЕЛЬНО ЗАМЕНИТЬ НА РЕАЛЬНЫЙ)
+is_testmode = False      # Флаг тестового режима
 is_debugmode = True     # Флаг вывода отладочных сообщений
-off_dispatcher = False  # Флаг отключения диспетчера движения
-host = '0.0.0.0'        # Сервер локальный
-port = '8080'           # Порт универсальный для всех серверов этого проекта
-tank_volume = 25000     # Вместимость топливозаправщика (в литрах)
-max_trucks = 3          # Максимальное количество топливозаправщиков
+dispatcher = False      # Флаг отключения диспетчера движения
+host = '0.0.0.0'        # Сервер локальныйr
+port = '5555'           # Порт универсальный для всех серверов этого проекта
+tank_volume = 10000     # Вместимость топливозаправщика (в литрах)
+max_trucks = 4          # Максимальное количество топливозаправщиков
 total_trucks = 0        # Общее количество созданных топливозаправщиков
-garagepoint = 297       # Точка гаража. Эту цифру надо взять у разработчика Диспетчера движения
-garage_free = True      # Индикатор свободного места у гаража
+garagepoint = 300       # Точка гаража. Эту цифру надо взять у разработчика Диспетчера движения
 protokol = "http://"    # Протокол
 gas = 'gas'             # Название в URL объекта "Заправка"
 plane = 'plane'         # Название в URL объекта "Самолет"
@@ -25,6 +24,10 @@ garage = 'garage'       # Название в URL объекта "Гараж"
 fueltruck = 'fueltruck' # Название в URL объекта "Гараж"
 dr = "dr"               # Название в URL объекта "Диспетчер руления"
 uno = "uno"             # Название в URL объекта "Управление наземными операциями"
+frequency = 1           # Частота хождения по маршруту в секундах
+loginfo = 'Лог:'        # Глобальная переменная, хранящая последнее сообщение
+total_orders = 0        # Общее количество выполненных заказов
+orders = []             # Массив заказов
 
 class FuelTruck:
     def __init__(self, nomer, plane_id, volume_plane, order_no): # Конструктор класса: plane_id - id самолета; volume_plane - объем бака самолета; order_no - номер заказа
@@ -50,15 +53,8 @@ class FuelTruck:
 
 
     def do_mission(self):     # Метод выполняет работу заправщика
-        global garage_free
-
-        if self.current_checkpoint == garagepoint:                      # Если текущая точка - гараж, то ставим отметку - Занято
-            garage_free = False                                         # Гараж занят 
-        else:
-            garage_free = True                                          # Гараж свободен 
-
         while self.busy == 1:                                           # Цикл до тех пор, пока бензовоз занят
-            time.sleep(1)                                               # Задержка в 1 секунду
+            time.sleep(frequency)                                       # Задержка в 1 секунду
 
             if self.moving_to_target_is_done():                         # Если успешно перешли к цели (обновили поле current_place)
                 if self.current_place == plane:                         # Если бензовоз у самолета
@@ -79,7 +75,10 @@ class FuelTruck:
                     self.send_mission_complete()                        # Отправляем сообщение в УНО - миссия завершена
 
                 elif self.current_place == gas:                         # Если бензовоз у заправки
-                    self.full = tank_volume                             # Бензовоз заправлен
+                    if self.volume_plane >= tank_volume:                # Если емкость самолета бльше бензовоза
+                        self.full = tank_volume                         # Бензовоз заправлен полностью
+                    else:
+                        self.full = self.volume_plane                   # Иначе заправляем на величину емкости самолета
 
             self.set_next_target_place()                                # Назначаем новый целевой объект (обновляем поле next_target_place)
 
@@ -87,58 +86,45 @@ class FuelTruck:
     def get_checkpoint_massiv(self): # Метод запрашивает маршрут до целевого объекта
         checkpoints = [] # Пустой массив чекпойнтов
 
-        if is_testmode or off_dispatcher: # Заглушка для режима тестирования
+        if  dispatcher: # Заглушка для режима тестирования
             checkpoints = [1, 2, 3, 4, 5]
             return checkpoints
-       
+
         if self.next_target_place == plane:
-            url = f"{protokol}{ip_dr}:{port}/{self.next_target_place}/{self.current_checkpoint}/{self.plane_id}"
+            url = f"{protokol}{ip_dr}:{port}/dispatcher/{self.next_target_place}/{self.current_checkpoint}/{self.plane_id}"
         else:
-            url = f"{protokol}{ip_dr}:{port}/{self.current_checkpoint}/{self.next_target_place}"
- 
-        # Запрос маршрута. Расшифровка URL:
-        # current_checkpoint - Номер текущей точки (число на сетке)
-        # next_target_place - Название целевого объекта (самолет, заправка, гараж)
-        # plane_id - Номер площадки самолета
-        dbg(f"Отправка: {url}")
-        response = requests.get(url) # Отправка запроса
+            url = f"{protokol}{ip_dr}:{port}/dispatcher/{self.current_checkpoint}/{self.next_target_place}/"
 
-        if response.status_code == 200: # Если ответ получен то
-            checkpoints = response.json() # Преобразуем json в массив
-        else:
-            dbg(f"Ошибка запроса пути: {url} {response.status_code}")        
- 
-        return checkpoints
+        while True: # Бесконечный цикл запроса пути до цели
+            time.sleep(0.1)
+            dbg(f"Отправка запроса пути: {url}")
+            response = requests.get(url) # Отправка запроса
 
+            if response.status_code == 200: # Если ответ получен то
+                checkpoints = response.json() # Преобразуем json в массив
+                return checkpoints
+            else:
+                dbg(f"Ошибка запроса пути: {url} {response.status_code}")        
+ 
 
     def ask_next_point(self): # Запрос следующей точки
-        global garage_free
-
-        if is_testmode or off_dispatcher: # Заглушка для режима тестирования
-            garage_free = True
+        if dispatcher:        # Заглушка для режима тестирования
             return True
 
-        url = f"{protokol}{ip_dr}:{port}/point/{self.current_checkpoint}/{self.next_checkpoint}"
-        # Запрос на разрешение движения к контрольной точке. Расшифровка URL:
-        # dr - Запрос к диспетчеру руления
-        # point - Находимся на точке
-        # current_checkpoint - Номер текущей точки (число на сетке)
-        # next_checkpoint - Номер следующей точки (число на сетке)
-        dbg(f"Отправка: {url}")
-        response = requests.get(url)                       # Отправка запроса
+        url = f"{protokol}{ip_dr}:{port}/dispatcher/point/{self.current_checkpoint}/{self.next_checkpoint}"
 
-        if response.status_code == 200:                    # Если ответ получен то
-            if response.text == 'true':                    # Латинские символы
-                if self.current_checkpoint == garagepoint: # Если текущей была точка гаража
-                    garage_free = True                     # Освобождаем точку гаража
+        while True:  # Бесконечный цикл запроса чекпойнта
+            time.sleep(0.1)
+            dbg(f"Отправка запроса чекпойнта: {url}")
+            response = requests.get(url)                       # Отправка запроса
 
-                return True
+            if response.status_code == 200:                    # Если ответ получен то
+                if response.text == 'true':                    # Латинские символы
+                    return True
+                else:
+                    return False
             else:
-                return False
-        else:
-            dbg(f"Ошибка запроса чекпойнта: {url} {response.status_code}")
-    
-        return False
+                dbg(f"Ошибка запроса чекпойнта: {url} {response.status_code}")
 
 
     def moving_to_target_is_done(self):                   # Этот метод отвечает за движение от одного объекта до другого
@@ -212,20 +198,23 @@ class FuelTruck:
         if is_testmode: # Заглушка для режима тестирования
             return 
 
-        url = f"{protokol}{ip_plane}:{port}/{fueltruck}/{plane}/{self.plane_id}/success"
+        url = f"{protokol}{ip_plane}:{port}/refuel/{self.plane_id}"
 
         dbg(f"Отправка: {url}")
         response = requests.get(url)
         if response.status_code == 200: # Если ответ получен то
            dbg(f"Успешно: {url}")
+           return
         else:
            dbg(f"Ошибка отправки success: {url} {response.status_code}")
 
 
     def send_mission_complete(self):  # Метод отправки сообщения о завершении миссии в УНО
+        global total_orders
         dbg("Уведомление УНО")
 
         if is_testmode: # Заглушка для режима тестирования
+            total_orders = total_orders + 1
             return 
 
         url = f"{protokol}{ip_uno}:{port}/{uno}/api/v1/order/successReport/{self.order_no}/tanker-truck"
@@ -234,6 +223,8 @@ class FuelTruck:
         response = requests.get(url)
         if response.status_code == 200: # Если ответ получен то
            dbg(f"Успешно: {url}")
+           total_orders = total_orders + 1
+           return
         else:
            dbg(f"Ошибка отправки в УНО: {url} {response.status_code}")
 
@@ -251,12 +242,12 @@ def create_app():
     @app.route('/fueltruck/order/<int:order_no>/<int:volume_plane>/<int:plane_id>')
     def serve_order(order_no, volume_plane, plane_id):                            # Процедура приема заказа
         dbg(f"Прием заказа: {order_no}, {volume_plane}, {plane_id}")
-        global total_trucks 
+        global total_trucks, orders
 
         if total_trucks < max_trucks:                                             # Если не все бензовозы созданы
             wait = 0
             while not garage_free and wait <= 10000:                              # Включаем режим ожидания освобождения точки гаража
-                time.sleep(0.1)
+                time.sleep(1)
                 wait = wait + 1
 
             total_trucks = total_trucks + 1                                       # Наращиваем количество бензовозов
@@ -278,11 +269,14 @@ def create_app():
                         dbg(f"Бензовоз №{index} найден: {order_no}, {volume_plane}, {plane_id}")
                         wait = 0
                         while not garage_free and wait <= 10000:                      # Включаем режим ожидания освобождения точки гаража
-                            time.sleep(0.1)
+                            time.sleep(1)
                             wait = wait + 1
 
-                        truck.new_mission(index, plane_id, volume_plane, order_no)    # Запуск новой миссии
-                        dbg(f"Бензовоз №{index} запущен: {order_no}, {volume_plane}, {plane_id}")
+                        if order_no not in orders:                                    # Проверяем, в работе ли заказ
+                            orders.append(order_no)                                   # Если не в работе то добавляем список в работе 
+                            truck.new_mission(index, plane_id, volume_plane, order_no)    # Запуск новой миссии
+                            dbg(f"Бензовоз №{index} запущен: {order_no}, {volume_plane}, {plane_id}")
+
                         return "success"
  
                     index = index + 1                                                 # Наращиваем счетчик
@@ -292,13 +286,57 @@ def create_app():
             dbg(f"Бензовоз не найден: {order_no}, {volume_plane}, {plane_id}, попыток {cnt}")
             return "Заказ не выполнен: Бензовоз не найден" 
         return f"Заказ выполнен: {order_no}, {volume_plane}, {plane_id}"
+
+    @app.route('/gas', methods=['GET', 'POST'])
+    def gas():
+        global max_trucks, total_trucks, frequency, loginfo, tank_volume, total_orders
+
+        if request.method == 'POST':
+            # Обновление переменных на основе данных формы
+            max_trucks = int(request.form['max_trucks'])
+            #total_trucks = int(request.form['total_trucks'])
+            frequency = int(request.form['frequency'])
+            #loginfo = int(request.form['loginfo'])
+            tank_volume = int(request.form['tank_volume'])
+
+        # Отображение HTML-страницы с текущими значениями переменных
+        return render_template("fueltruck2.html", max_trucks=max_trucks, total_trucks=total_trucks, frequency=frequency, loginfo=loginfo, tank_volume=tank_volume, total_orders=total_orders)
     return app
+
 
 def dbg(word): # Процедура выводит в консоль отладочные сообщения
     if is_debugmode:
+        global loginfo
+        
+        if len(loginfo) >= 32767:
+            loginfo = word
+        else:    
+            loginfo = loginfo + "\n" + word
+
         print(word)
 
 fueltrucks = defaultdict(FuelTruck)  # Массив бензовозов
+
+
+def garage_free():        # Запрос доступности точки гаража
+    if dispatcher:        # Заглушка для режима тестирования
+        return True
+
+    url = f"{protokol}{ip_dr}:{port}/dispatcher/garage/fuel_truck"
+
+    while True: # Бесконечный цикл запроса материализации в гараже
+        time.sleep(0.1)
+        dbg(f"Отправка: {url}")
+        response = requests.get(url)                       # Отправка запроса
+
+        if response.status_code == 200:                    # Если ответ получен то
+            if response.text == 'true':                    # Латинские символы
+                return True
+            else:
+                return False
+        else:
+            dbg(f"Ошибка запроса точки гаража: {url} {response.status_code}")
+
 
 def main():
     app = create_app()
